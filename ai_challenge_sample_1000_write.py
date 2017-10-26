3caa01d1513594b915aba9ae7cbf582a33b4c5ee
gongniuxiaodong <1247721601@qq.com>
2017-10-26 18:14:59 +0800

    add ai sample 1000 part

--- ai_challenge_sample_1000_write.py
+++ ai_challenge_sample_1000_write.py
@@ -20,19 +20,24 @@ for filename in os.listdir(r"D:\搜狗高速下载\ai_challenger_caption_train_2
        break
 # print(len(index))
 # print(fileName)
-for i in range(0,1000):
-    data_image_id_1000.append(data[index[i]])
-# print(data_image_id_1000)
-write_json = json.dumps(data_image_id_1000)
-with open('d:/ai_challenge_picture_sample_1000/json_data.json','w') as json_file:
-    json_file.write(write_json)
+# for i in range(0,1000):
+#     data_image_id_1000.append(data[index[i]])
+# # print(data_image_id_1000)
+# write_json = json.dumps(data_image_id_1000)
+# with open('d:/ai_challenge_picture_sample_1000/json_data.json','w') as json_file:
+#     json_file.write(write_json)
 import matplotlib.pyplot as plt # plt 用于显示图片
 import matplotlib.image as mpimg # mpimg 用于读取图片
 import numpy as np
-for filename in fileName:
-    lena = mpimg.imread("D:/搜狗高速下载/ai_challenger_caption_train_20170902/ai_challenger_caption_train_20170902/caption_train_images_20170902"+'/'+filename)
-    # plt.savefig('d:/ai_challenge_picture_sample_1000/picture/'+filename)//打印好的图像才能保存
-    np.save('d:/ai_challenge_picture_sample_1000/picture/'+filename,lena)
+# for filename in fileName:
+#     lena = mpimg.imread("D:/搜狗高速下载/ai_challenger_caption_train_20170902/ai_challenger_caption_train_20170902/caption_train_images_20170902"+'/'+filename)
+#     # plt.savefig('d:/ai_challenge_picture_sample_1000/picture/'+filename)//打印好的图像才能保存
+#     np.save('d:/ai_challenge_picture_sample_1000/picture/'+filename,lena)
 # plt.imshow(lena)  # 显示图片
 # plt.axis('off')  # 不显示坐标轴
 # plt.show()
+import shutil
+for filename in fileName:
+    old_path = "D:/搜狗高速下载/ai_challenger_caption_train_20170902/ai_challenger_caption_train_20170902/caption_train_images_20170902/"+filename
+    new_path = 'd:/ai_challenge_picture_sample_1000/picture1/'+filename
+    shutil.copyfile(old_path,new_path)
